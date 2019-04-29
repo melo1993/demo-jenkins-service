ecb3b2d299d6a3269e1b2bbcf4ea5c5716bc9738
melo <melo.mei@dadaabc.com>
2019-04-29 17:40:34 +0800

    demo-jenkins-service-gradle

---
+++ src/test/java/com/example/demo/DemoJenkinsServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RestController
+public class TestController {
+	@GetMapping("/index")
+	public Map<String, String> index()
+	{
+		Map<String, String> result = new HashMap<>();
+		result.put("test1", "test1");
+		result.put("test2", "test2");
+		result.put("test3", "test3");
+		return result;
+	}
+}
