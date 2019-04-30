02917ff4035640938f4f1a65d0732d0bfdc1a283
melo <melo.mei@dadaabc.com>
2019-04-30 10:51:23 +0800

    demo-jenkins-service-gradle

--- src/main/java/com/example/demo/Controller/TestController.java
+++ src/main/java/com/example/demo/Controller/TestController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
