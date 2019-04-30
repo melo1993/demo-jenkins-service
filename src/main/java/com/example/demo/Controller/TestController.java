17548aa699c8baec95f9874caaa557668a1191a5
melo <melo.mei@dadaabc.com>
2019-04-30 14:24:07 +0800

    demo-jenkins-service-gradle

--- src/main/java/com/example/demo/Controller/TestController.java
+++ src/main/java/com/example/demo/Controller/TestController.java
@@ -1,13 +1,18 @@
-package com.example.demo;
+package com.example.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/index")
+
 public class TestController {
 	@GetMapping("/index")
-	public Map<String, String> index()
-	{
+	public Map<String, String> index(){
 		Map<String, String> result = new HashMap<>();
 		result.put("test1", "test1");
 		result.put("test2", "test2");
--- src/test/java/com/example/demo/DemoJenkinsServiceApplicationTests.java
+++ src/test/java/com/example/demo/DemoJenkinsServiceApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
